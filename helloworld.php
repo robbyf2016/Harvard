89fe4875f2568c4e1452cddb83412cf62723ffd5
Robby Fussell Harvard <robbyfussell@fas.harvard.edu>
2014-09-08 09:47:40 -0400

    Initial commit
    Initial commit of Helloworld PHP

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<!--This is for CSCIE15 Dynamic Web Applications Class Fall 2014 Harvard-->
+<title>Hello World</title>
+<meta charset='utf-8'>
+</head>
+<body>
+<?php
+echo 'Hello World! You are visiting ';
+echo $_SERVER['SERVER_NAME'];
+?>
+</body>
+</html>

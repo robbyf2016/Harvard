9f43588949b9a157e50e2b1ff924b6747be68249
Robby Fussell Harvard <robbyfussell@fas.harvard.edu>
2014-09-08 10:06:26 -0400

    This patch adds comments and new title
    Adds new page title and comments within the code

--- helloworld.php
+++ helloworld.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <!--This is for CSCIE15 Dynamic Web Applications Class Fall 2014 Harvard-->
-<title>Hello World</title>
-<meta charset='utf-8'>
+<!--Initial assignment to add PHP code to GitHub Repository-->
+<!--Validated for XHTML 1.0 Transitional Complete-->
+<title>Hello World - Robby Fussell @ Harvard</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <?php

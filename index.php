dc5ccc013a033c6df2970509ff36f01c8b8faf2a
Robby Fussell Harvard <robbyfussell@fas.harvard.edu>
2014-09-11 15:08:51 -0400

    Added index.php for helloworld.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Hello World HTML</title>
+<title>Untitled Document</title>
 </head>
+
 <body>
-<?php include ('helloworld.php')?>
+<?php include("helloworld.php"); ?>
 </body>
 </html>

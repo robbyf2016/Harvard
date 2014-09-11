f9a54f608c74697aad4be48cb263f4098a187e68
Robby Fussell Harvard <robbyfussell@fas.harvard.edu>
2014-09-11 16:36:36 -0400

    Modified helloworld.php file to coincide with W3C reqs

--- helloworld.php
+++ helloworld.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!--This is for CSCIE15 Dynamic Web Applications Class Fall 2014 Harvard-->
-<!--Initial assignment to add PHP code to GitHub Repository-->
-<!--Validated for XHTML 1.0 Transitional Complete-->
-<title>Hello World - Robby Fussell @ Harvard</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-<body>
 <?php
 echo 'Hello World! You are visiting ';
 echo $_SERVER['SERVER_NAME'];
@@ -16,5 +6,3 @@ echo $_SERVER['SERVER_NAME'];
     <a href="http://validator.w3.org/check?uri=referer"><img
       src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
 </p>
-</body>
-</html>
--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Hello World of CSCIE-15</title>
 </head>
-
 <body>
 <?php include("helloworld.php"); ?>
 </body>
